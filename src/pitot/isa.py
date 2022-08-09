8fafec497a26b936af44847da48d5c0a49d428ce
Xavier Olive <git@xoolive.org>
2022-08-10 00:42:10 +0200

    minimal documentation

--- src/pitot/isa.py
+++ src/pitot/isa.py
@@ -21,6 +21,14 @@ STRATOSPHERE_TEMP = Q_(216.65, ureg.K)  # until altitude = 22km
 
 @default_units(h=ureg.meter)
 def temperature(h: Any) -> pint.Quantity[Any]:
+    """Temperature of ISA atmosphere
+
+    :param h: the altitude (by default in meters), :math:`0 < h < 84852`
+        (will be clipped when outside range, integer input allowed)
+
+    :return: the temperature (in K)
+
+    """
     temp = np.maximum(
         Q_(288.15, "K") - Q_(0.0065, "K/m") * h,
         STRATOSPHERE_TEMP,
@@ -30,6 +38,14 @@ def temperature(h: Any) -> pint.Quantity[Any]:
 
 @default_units(h=ureg.meter)
 def density(h: Any) -> pint.Quantity[Any]:
+    """Density of ISA atmosphere
+
+    :param h: the altitude (by default in meters), :math:`0 < h < 84852`
+        (will be clipped when outside range, integer input allowed)
+
+    :return: the density :math:`\\rho`, in kg/m3
+
+    """
     temp = temperature(h)
     density_troposphere = RHO_0 * (temp / Q_(288.15, ureg.K)) ** 4.256848
     delta = np.maximum(Q_(0, ureg.meter), h - Q_(11000, ureg.meter))
@@ -41,6 +57,14 @@ def density(h: Any) -> pint.Quantity[Any]:
 
 @default_units(h=ureg.meter)
 def pressure(h: Any) -> pint.Quantity[Any]:
+    """Pressure of ISA atmosphere
+
+    :param h: the altitude (by default in meters), :math:`0 < h < 84852`
+        (will be clipped when outside range, integer input allowed)
+
+    :return: the pressure, in Pa
+
+    """
     temp = temperature(h)
     den = density(h)
     press: pint.Quantity[Any] = den * temp * SPECIFIC_GAS_CONSTANT
@@ -49,6 +73,14 @@ def pressure(h: Any) -> pint.Quantity[Any]:
 
 @default_units(h=ureg.meter)
 def sound_speed(h: Any) -> pint.Quantity[Any]:
+    """Speed of sound in ISA atmosphere
+
+    :param h: the altitude (by default in meters), :math:`0 < h < 84852`
+        (will be clipped when outside range, integer input allowed)
+
+    :return: the speed of sound :math:`a`, in m/s
+
+    """
     temp = temperature(h)
     a = np.sqrt(GAMMA * R * temp)
     return a  # type: ignore
