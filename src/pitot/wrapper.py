32fa1026bd87eeefa0d458234ea6ee81fe4a38e7
achevrot <antoine.chevrot@onera.fr>
2022-11-14 15:22:00 +0100

    it's couscous time !

--- src/pitot/wrapper.py
+++ src/pitot/wrapper.py
@@ -1,15 +1,19 @@
 from __future__ import annotations
 
+import ast
 import functools
 import inspect
 import logging
+import textwrap
 from typing import Any, Callable
 
+import astor
 import pandas as pd
 import pint
 from pint_pandas import PintArray, PintType
 
 from . import Q_
+from .couscous import Visitor
 
 ReturnQuantity = Callable[..., pint.Quantity]
 
@@ -57,3 +61,41 @@ def default_units(**unit_kw: str | pint.Unit) -> Callable[..., ReturnQuantity]:
         return decorated_func
 
     return wrapper
+
+
+def couscous(fun: Callable) -> Callable:
+    """Decorator function to check units based on annotations
+
+    Args:
+        fun (function): decorated function
+
+    Returns:
+        new_fun (function): new function based on input function with eventually modified
+        code to keep unit coherence.
+    """
+
+    fun_tree = ast.parse(
+        textwrap.dedent(inspect.getsource(fun))  # dedent for nested methods
+    )  # get the function AST
+    visitor = Visitor(fun.__globals__)
+    fun_tree = visitor.visit(fun_tree)  # send it to the NodeTransformer
+    f_str = astor.to_source(
+        fun_tree
+    )  # get the string of the transformed function
+    _log.warning(f_str)
+    exec(f_str[f_str.find("\n") + 1 :], fun.__globals__, locals())
+
+    new_fun = locals()[fun.__name__]
+
+    co_consts = new_fun.__code__.co_consts
+    for const in fun.__code__.co_consts:
+        if const not in co_consts:
+            co_consts = co_consts + (const,)
+    cocode = new_fun.__code__.co_code
+
+    fun.__code__ = fun.__code__.replace(
+        co_code=cocode,
+        co_consts=co_consts,
+    )
+
+    return fun
---
+++ tests/couscous/test_calculations.py
@@ -0,0 +1,42 @@
+from typing import Any
+import pytest
+from pitot.wrapper import couscous
+
+m = K = ft = Any
+
+
+def test_mixed_units():
+    @couscous
+    def test_mixed_units():
+        alt_1: "m" = 1000
+        alt_2: "ft" = 350
+        temp: "K" = 120
+        result: "m / K" = (alt_1 + alt_2) * 25 / temp
+        print(result)
+        assert result == pytest.approx(230.55, rel=1e-2)
+
+    test_mixed_units()
+
+
+def test_add_operation():
+    @couscous
+    def test_operation():
+        alt_m: "m" = 1000
+        alt_ft: "ft" = 2000
+        alt_m2: "m" = 3000
+        result: "m" = alt_m + alt_ft + alt_m2
+        assert result == pytest.approx(4609.6, rel=1e-2)
+
+    test_operation()
+
+
+def test_list_operation():
+    @couscous
+    def test_operation():
+        alt_m: "m" = 1000
+        alt_ft: "ft" = 2000
+        alt_m2: "m" = 3000
+        _, result_2 = (alt_m + alt_ft + alt_m2, alt_m + alt_ft + alt_m2)
+        assert result_2 == pytest.approx(4609.6, rel=1e-2)
+
+    test_operation()
