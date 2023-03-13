5f5f98b6516a68eb8dc94345e6187a45df7a8fe8
Xavier Olive <git@xoolive.org>
2023-03-13 16:27:28 +0100

    new tests, changes necessary in impunity

--- tests/test_aero.py
+++ tests/test_aero.py
@@ -3,9 +3,10 @@ from __future__ import annotations
 import pytest
 from impunity import impunity
 from pitot import aero
-
-# import numpy as np
 from typing_extensions import Annotated
+
+import numpy as np
+import numpy.typing as npt
 
 # def test_aero() -> None:
 
@@ -13,15 +14,28 @@ from typing_extensions import Annotated
 @impunity
 def test_mach2tas() -> None:
     # https://fr.wikipedia.org/wiki/Nombre_de_Mach
-    alt: Annotated[list[float], "m"] = [0, 1000]
+    alt: Annotated[npt.NDArray[np.int64], "m"] = np.array([0, 1000])
     r1 = aero.mach2tas(1, alt)
-    r2: Annotated[list[float], "kts"] = [340.3, 336.4]
+    r2: Annotated[npt.NDArray[np.float64], "kts"] = np.array([340.3, 336.4])
     assert r1 == pytest.approx(r2, rel=1e-2)
 
 
-# # https://en.wikipedia.org/wiki/Mach_number
-# r2 = aero.mach2tas([0.8, 1.2, 5, 10], 0)
-# assert r2 == pytest.approx([530, 794, 3308, 6615], rel=1e-2)
+@impunity(rewrite="test_mach2tas_part2.py")
+def test_mach2tas_part2() -> None:
+    # https://en.wikipedia.org/wiki/Mach_number
+    mach: Annotated[npt.NDArray[np.float64], "dimensionless"] = np.array(
+        [0.8, 1.2, 5, 10]
+    )
+    # alt_zero: "m" = 0  # TODO
+    alt_zero = 0
+    r3 = aero.mach2tas(mach, alt_zero)
+    expected_r3: Annotated[npt.NDArray[np.float64], "kts"] = np.array(
+        [530, 794, 3308, 6615]
+    )
+    print(r3)
+    print(expected_r3)  # check units
+    assert r3 == pytest.approx(expected_r3, rel=1e-2)
+
 
 # r3 = aero.tas2mach(r2, 0)
 # assert r3 == pytest.approx([0.8, 1.2, 5, 10])
